ad2e36f27e5646cb4a5c8c34db176d7c9ea8e677
Francisco d'Albertas <francisco.albertas@gmail.com>
2021-11-09 14:47:10 -0300

    mantive apenas plots com ggplot

--- export_flow.R
+++ export_flow.R
@@ -27,35 +27,6 @@ trase <- read.csv("exports_codigo_ibg.csv")
 trase_agg <- trase %>% group_by(STATE,COUNTRY,TYPE)%>%
   summarise(total_land=sum(land_use_ha),total_prod=sum(production))
 
-# tem q ser 0 indexed, ou seja comecar do 0
-trase_agg$source <- as.integer(as.factor(trase_agg$STATE))
-trase_agg$source <- trase_agg$source-1
-max(trase_agg$source)
-# tem q ser de 9 pra cima
-trase_agg$target <- as.integer(as.factor(trase_agg$COUNTRY))
-min(trase_agg$target)# aqui tem q somar 9 pra dar 10
-trase_agg$target <- trase_agg$target+9
-
-nos1 <- data.frame(unique(trase_agg[,1]))
-nos2 <- data.frame(unique(trase_agg[,2]))
-
-names(nos1) <- "name"
-names(nos2) <- "name"
-
-nos <-rbind(nos1,nos2)
-
-trase_list <- list(links=as.data.frame(trase_agg[,c(6:7,5)]),nodes=nos)
-
-head(trase_list[[1]])
-head(trase_list[[2]])
-str(trase_list[[1]])
-str(trase_list[[2]])
-
-
-
-sankeyNetwork(Links = trase_list$links, Nodes =trase_list$nodes, Source = "source",
-              Target = "target", Value = "total_prod", NodeID = "name",
-               fontSize = 14)
 
 #===============================================================================
 
@@ -67,29 +38,6 @@ trase_agg <- trase %>% group_by(STATE,COUNTRY,TYPE,YEAR)%>%
 
 trase_agg <- trase_agg[trase_agg$YEAR==2015,]  
 
-# tem q ser 0 indexed, ou seja comecar do 0
-trase_agg$source <- as.integer(as.factor(trase_agg$STATE))
-trase_agg$source <- trase_agg$source-1
-max(trase_agg$source)
-# tem q ser de 8 pra cima
-trase_agg$target <- as.integer(as.factor(trase_agg$COUNTRY))
-min(trase_agg$target)# aqui tem q somar 9 pra dar 10
-trase_agg$target <- trase_agg$target+8
-
-nos1 <- data.frame(unique(trase_agg[,1]))
-nos2 <- data.frame(unique(trase_agg[,2]))
-
-names(nos1) <- "name"
-names(nos2) <- "name"
-
-nos <-rbind(nos1,nos2)
-
-trase_list <- list(links=as.data.frame(trase_agg[,c(7,8,6)]),nodes=nos)
-
-head(trase_list[[1]])
-head(trase_list[[2]])
-str(trase_list[[1]])
-str(trase_list[[2]])
 
 # https://cran.r-project.org/web/packages/ggalluvial/vignettes/ggalluvial.html
 
